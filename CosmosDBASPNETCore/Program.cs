32689021662a845a3cd100bd98a0e39969ec53ad
avnish1981 <avnish.choubey@gmail.com>
2020-05-27 16:37:04 +0530

    Add project files.

---
+++ CosmosDBASPNETCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CosmosDBASPNETCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
