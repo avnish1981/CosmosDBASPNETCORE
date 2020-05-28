d0c541bd1201b09818a6f6bd215bbbeff1204ff6
avnish1981 <avnish.choubey@gmail.com>
2020-05-28 14:10:08 +0530

    Initail Commit

--- CosmosDBASPNETCore/Program.cs
+++ CosmosDBASPNETCore/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
 
 namespace CosmosDBASPNETCore
 {
@@ -6,7 +9,41 @@ namespace CosmosDBASPNETCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            QueryForDocument().Wait();
+
+
+        }
+        private async static Task QueryForDocument()
+        {
+            try
+            {
+                var config = new ConfigurationBuilder().AddJsonFile("App.json").Build();
+                var endpoint = config["CosmosDBEndPoint"];
+                var masterkey = config["CosmosDBMasterKey"];
+
+                using (var client = new CosmosClient(endpoint, masterkey))
+                {
+                    var container = client.GetContainer("Families", "Families");
+                    var sql = "SELECT * FROM c ";
+                    var iterator = container.GetItemQueryIterator<dynamic>(sql);
+                    var page = await iterator.ReadNextAsync();
+                    foreach (var doc in page)
+                    {
+                        Console.WriteLine($"Family Name { doc.familyName} has Address {doc.address.zipCode} ");
+                        Console.ReadLine();
+                    }
+                }
+
+            }
+            catch(Exception ex)
+            {
+                string str = ex.Message;
+            }
+            finally
+            {
+
+            }
+            
         }
     }
 }
